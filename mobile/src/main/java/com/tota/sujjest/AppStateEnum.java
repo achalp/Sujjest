131423b3b5de5e91824caaba904036c3ab3e29f3
achalp <achalp@gmail.com>
2015-12-27 16:11:53 -0800

    Lots og bug fixes for location, geo coding and refreshing the map markers. Still quite messy but as long as you stay in landscape mode, it should work.

---
+++ mobile/src/main/java/com/tota/sujjest/AppStateEnum.java
@@ -0,0 +1,9 @@
+package com.tota.sujjest;
+
+/**
+ * Created by aprabhakar on 12/26/15.
+ */
+public enum AppStateEnum {
+    HOME_SCREEN, MAPVIEW_SCREEN,REREIVING_REVIEWS_SCREEN,RECOMMENDATION_SCREEN
+
+}
